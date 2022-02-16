e310f72bbebfc5b438fc255674a64e4d4f550e60
csallo.mate <csallo.mate@gmail.com>
2022-02-16 21:27:25 +0100

    Refactor for remote connection with fix extension

--- src/main/java/com/codecool/fileshare/repository/ImageJdbcRepository.java
+++ src/main/java/com/codecool/fileshare/repository/ImageJdbcRepository.java
@@ -1,12 +1,16 @@
 package com.codecool.fileshare.repository;
 
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,51 +19,57 @@ import java.util.UUID;
 
 @Component("jdbc")
 public class ImageJdbcRepository implements ImageRepository{
+
     private final DataSource dataSource;
 
     public ImageJdbcRepository(){
-        dataSource=initDataSource();
-        System.out.println(dataSource);
+        this.dataSource=initDataSource();
     }
 
     @Override
     public String storeImage(String category, String content)  {
         System.out.println(category+"\n"+content);
-        String sql="INSERT INTO art_gallery VALUES(?,?,?)";
-        String uuid="";
+        String sql="INSERT INTO image (id,category,content,extension) VALUES(?,?,?,?)";
+        UUID uuid=null;
         try {
-            PreparedStatement ps = dataSource.getConnection().prepareStatement(sql);
+            Connection conn=dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
             uuid=uuidFromBase64(content);
-            ps.setString(1,uuid);
+            ps.setObject(1,uuid);
             ps.setString(2,category);
             ps.setBytes(3,content.getBytes(StandardCharsets.UTF_8));
+            ps.setString(4,"jpg");
             ps.execute();
-            ps.getConnection().close();
+            ps.close();
+            conn.close();
         } catch (SQLException e){
             e.printStackTrace();
         }
-        return uuid;
+        assert uuid != null;
+        return uuid.toString();
     }
 
-    private String uuidFromBase64(String str) {
+    private UUID uuidFromBase64(String str) {
         byte[] bytes = Base64.decodeBase64(str);
         ByteBuffer bb = ByteBuffer.wrap(bytes);
-        UUID uuid = new UUID(bb.getLong(), bb.getLong());
-        return uuid.toString();
+        return new UUID(bb.getLong(), bb.getLong());
     }
 
     @Override
     public String readImage(String uuid) {
-        String sql="SELECT image from art_gallery WHERE uuid=?;";
+        String sql="SELECT image from image WHERE id::varchar=?;";
         String forTheReturn="";
         try {
-            PreparedStatement ps = dataSource.getConnection().prepareStatement(sql);
+            Connection conn=dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1,uuid);
             ResultSet rs = ps.executeQuery();
             rs.next();
             byte[] bytes=rs.getBytes(1);
             forTheReturn=new String(bytes, StandardCharsets.UTF_8);
-            ps.getConnection().close();
+            rs.close();
+            ps.close();
+            conn.close();
         } catch (SQLException e){
             e.printStackTrace();
         }
