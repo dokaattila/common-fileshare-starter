6b9c038da2d3a948f00cd794af9bf9b57b605210
HereticWay <vpleft@gmail.com>
2022-02-17 17:15:16 +0100

    update init.sql;

--- init.sql
+++ init.sql
@@ -1,10 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE image (
-    id uuid DEFAULT uuid_generate_v4(),
-    category VARCHAR NOT NULL,
-    content BYTEA NOT NULL,
-    extension VARCHAR NOT NULL,
-    PRIMARY KEY (id)
+	id uuid NOT NULL DEFAULT uuid_generate_v4(),
+	category VARCHAR NOT NULL,
+	content BYTEA NOT NULL,
+	CONSTRAINT image_pkey PRIMARY KEY (id)
 );
+
 GRANT ALL PRIVILEGES ON TABLE image TO myuser;
