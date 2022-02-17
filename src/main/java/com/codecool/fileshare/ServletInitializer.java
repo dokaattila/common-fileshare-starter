2ff41d6ee0c1f3ff0c16c394d42b6e401fc60108
csallo.mate <csallo.mate@gmail.com>
2022-02-17 18:00:54 +0100

    Add .war

---
+++ src/main/java/com/codecool/fileshare/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.codecool.fileshare;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FileshareApplication.class);
+    }
+
+}
+
