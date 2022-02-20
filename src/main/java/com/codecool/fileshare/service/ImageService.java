aa4fed8cef4f082c681479521f4b8ec08b7ec279
dokaattila <dokaatti@gmail.com>
2022-02-20 14:19:26 +0100

    modify qualifier to file

--- src/main/java/com/codecool/fileshare/service/ImageService.java
+++ src/main/java/com/codecool/fileshare/service/ImageService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class ImageService {
 
     @Autowired
-    @Qualifier("jdbc")
+    @Qualifier("file")
     ImageRepository imageRepository;
 
     public String storeImage(ImageDTO imageDTO, String category){
