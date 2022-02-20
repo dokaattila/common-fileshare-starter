b7b2d1b59d2fe80b56f27f3a40e1da6e6b62590c
dokaattila <dokaatti@gmail.com>
2022-02-20 18:21:39 +0100

    implement read-write functions to file

--- src/main/java/com/codecool/fileshare/repository/ImageFileRepository.java
+++ src/main/java/com/codecool/fileshare/repository/ImageFileRepository.java
@@ -1,22 +1,44 @@
 package com.codecool.fileshare.repository;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Component;
 
-@Component("file")
-public class ImageFileRepository implements ImageRepository{
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
-    public String storeImage(String category, String content){
-        // Optional task: implement store in file here.
-        // different categories should be go to different folder.
-        // if you want this method to run instead of jdbc version find @Qualifier("jdbc") in
-        // service and change to @Qualifier("file")
-        return null;
+@Component("file")
+public class ImageFileRepository implements ImageRepository {
+
+    private final String path = "src/main/resources/files/";
+
+    public String storeImage(String category, String content) {
+        UUID uuid = generateUUID(content);
+        File file = new File(this.path + uuid);
+        try {
+            FileUtils.write(file, category + ":" + content + "\n", StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return uuid.toString();
     }
 
-    public String readImage(String uuid){
-        // Optional task: implement read from file here.
-        // if you want this method to run instead of jdbc version find @Qualifier("jdbc") in
-        // service and change to @Qualifier("file")
-        return null;
+    public String readImage(String uuid) {
+        File file = new File(this.path + uuid);
+        String[] readFileArray = new String[0];
+        try {
+            String readFile = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+            readFileArray = readFile.split(":");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return readFileArray[1];
+    }
+
+    private UUID generateUUID(String str) {
+        byte[] bytes = Base64.decodeBase64(str);
+        return UUID.nameUUIDFromBytes(bytes);
     }
 }
